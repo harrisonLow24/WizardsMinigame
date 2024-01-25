70f39b816151eef44e89e0cf2ad1cb9fd334b868
Harrison Low <harrison3060@gmail.com>
2024-01-25 06:48:48 -0500

    better spells menu with custom materal names & desc

--- src/main/java/WizardsGame/SpellBookMenu.java
+++ src/main/java/WizardsGame/SpellBookMenu.java
@@ -12,7 +12,12 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.Color;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class SpellBookMenu implements Listener {
@@ -22,7 +27,16 @@ public class SpellBookMenu implements Listener {
     }
 
     public void openSpellBookMenu(Player player) {
-        Inventory spellBookMenu = Bukkit.createInventory(null, 9, "Spell Book");
+        Inventory spellBookMenu = Bukkit.createInventory(null, 54, "Spell Book"); // Use 54 slots for a full chest
+
+        // fill empty slots with stained glass
+        ItemStack stainedGlass = new ItemStack(Material.GRAY_STAINED_GLASS_PANE, 1, (short) 15); // Use white stained glass
+        for (int i = 0; i < spellBookMenu.getSize(); i++) {
+            // set stained glass in specific slots, leaving gaps for spoells
+            if (i != 10 && i != 12 && i != 14 && i != 16 && i != 28 && i != 30 && i != 32 && i != 34) {
+                spellBookMenu.setItem(i, stainedGlass);
+            }
+        }
 
         // spell button creation with custom names
         ItemStack fireballButton = createSpellButton("Fiery Wand", Material.BLAZE_ROD);
@@ -33,17 +47,18 @@ public class SpellBookMenu implements Listener {
         ItemStack flyButton = createSpellButton("Winged Shield", Material.SHIELD);
         ItemStack GPButton = createSpellButton("Big Man Slam", Material.IRON_INGOT);
 
-        // spell buttons' positions
-        spellBookMenu.setItem(0, fireballButton);
-        spellBookMenu.setItem(1, teleportButton);
-        spellBookMenu.setItem(2, lightningButton);
-        spellBookMenu.setItem(3, minecartButton);
-        spellBookMenu.setItem(4, gustButton);
-        spellBookMenu.setItem(5, flyButton);
-        spellBookMenu.setItem(6, GPButton);
+        // spell buttons' positions, starting from the 9th slot (index 8)
+        spellBookMenu.setItem(10, fireballButton);
+        spellBookMenu.setItem(12, teleportButton);
+        spellBookMenu.setItem(14, lightningButton);
+        spellBookMenu.setItem(16, minecartButton);
+        spellBookMenu.setItem(28, gustButton);
+        spellBookMenu.setItem(30, flyButton);
+        spellBookMenu.setItem(32, GPButton);
 
         player.openInventory(spellBookMenu);
     }
+
 
     public ItemStack createSpellButton(String spellName, Material material) {
         ItemStack spellButton = new ItemStack(material);
@@ -52,11 +67,16 @@ public class SpellBookMenu implements Listener {
         ItemMeta meta = spellButton.getItemMeta();
         assert meta != null;
 
-        // Set custom name based on material
-        String customName = getCustomName(material);
-        meta.setDisplayName(ChatColor.RESET + customName);
+        // set custom name
+        ChatColor nameColor = getSpellNameColor(material);
+        String customName = nameColor + spellName;
+        meta.setDisplayName(customName);
 
-        // Additional customization if needed (lore, enchantments, etc.)
+        // set custom description
+        List<String> lore = getSpellLore(spellName);
+        meta.setLore(lore);
+
+        // note: can add additional customization(enchantments, etc.)
         spellButton.setItemMeta(meta);
 
         return spellButton;
@@ -68,16 +88,26 @@ public class SpellBookMenu implements Listener {
         String customName = getCustomName(material);
 
         assert meta != null;
-        meta.setDisplayName(ChatColor.RESET + customName);
+        meta.setDisplayName(getSpellNameColor(material) + customName); // Set custom name color
         spellItem.setItemMeta(meta);
 
-        // check if player's inventory is full
-        if (player.getInventory().firstEmpty() != -1) {
-            player.getInventory().addItem(spellItem);
-            player.sendMessage("You've acquired the spell: " + customName);
+        // set lore
+        List<String> lore = getSpellLore(customName);
+        meta.setLore(lore);
+        spellItem.setItemMeta(meta);
+
+        // check if player already has the spell
+        if (!hasSpell(player, customName)) {
+            // check if player's inventory is full
+            if (player.getInventory().firstEmpty() != -1) {
+                player.getInventory().addItem(spellItem);
+                player.sendMessage("You've acquired the spell: " + customName);
+            } else {
+                player.getWorld().dropItem(player.getLocation(), spellItem);
+                player.sendMessage("Your inventory is full. The spell: " + customName + " has been dropped at your location.");
+            }
         } else {
-            player.getWorld().dropItem(player.getLocation(), spellItem);
-            player.sendMessage("Your inventory is full. The spell: " + customName + " has been dropped at your location.");
+            player.sendMessage("You already have the spell: " + customName);
         }
     }
 
@@ -105,6 +135,79 @@ public class SpellBookMenu implements Listener {
         }
     }
 
+    private boolean hasSpell(Player player, String spellName) {
+        // check if player's inventory has item already
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && item.getType() != Material.AIR) {
+                ItemMeta meta = item.getItemMeta();
+                if (meta != null) {
+                    String itemName = ChatColor.stripColor(meta.getDisplayName());
+                    if (spellName.equals(itemName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public ChatColor getSpellNameColor(Material material) {
+        // custom colors for spells
+        switch (material) {
+            case BLAZE_ROD:
+                return ChatColor.RED;
+            case IRON_SWORD:
+                return ChatColor.BLUE;
+            case IRON_PICKAXE:
+                return ChatColor.YELLOW;
+            case MINECART:
+                return ChatColor.GREEN;
+            case FEATHER:
+                return ChatColor.WHITE;
+            case SHIELD:
+                return ChatColor.GOLD;
+            case IRON_INGOT:
+                return ChatColor.DARK_GRAY;
+            default:
+                return ChatColor.GRAY;
+        }
+    }
+
+    public List<String> getSpellLore(String spellName) {
+        // custom description for spells
+        List<String> lore = new ArrayList<>();
+        switch (spellName) {
+            case "Fiery Wand":
+                lore.add("Cast powerful fireballs.");
+                break;
+            case "Teleportation Blade":
+                lore.add("Teleport to a targeted location.");
+                break;
+            case "Thunder Hammer":
+                lore.add("Summon lightning with a mighty hammer swing.");
+                break;
+            case "Magical Minecart":
+                lore.add("Ride a magical minecart to travel quickly.");
+                break;
+            case "Gust Feather":
+                lore.add("Create a gust of wind to push away enemies.");
+                break;
+            case "Winged Shield":
+                lore.add("Ride away into the sunset.");
+                break;
+            case "Big Man Slam":
+                lore.add("Unleash a powerful ground slam as the BIG MAN you are.");
+                break;
+            default:
+                lore.add("Unknown Spell");
+                break;
+        }
+        return lore;
+    }
+
+
+
+
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
@@ -124,28 +227,40 @@ public class SpellBookMenu implements Listener {
         Player player = (Player) event.getWhoClicked();
         ItemStack clickedItem = event.getCurrentItem();
 
-        // check if clicked item is a spell button
+        // Check if clicked item is a spell button
         if (clickedItem != null && clickedItem.getType() != Material.AIR) {
             ItemMeta meta = clickedItem.getItemMeta();
+            if (clickedItem.getType() == Material.GRAY_STAINED_GLASS_PANE) {
+                // Prevent interaction with stained glass
+                event.setCancelled(true);
+                return;
+            }
             if (meta != null) {
                 String spellName = ChatColor.stripColor(meta.getDisplayName()); // Get spell name without color codes
 
-                // add spell to player's inventory
-                switch (spellName) {
-                    case "Fiery Wand":
-                    case "Teleportation Blade":
-                    case "Thunder Hammer":
-                    case "Magical Minecart":
-                    case "Gust Feather":
-                    case "Winged Shield":
-                    case "Big Man Slam":
-                        handleSpellAddition(player, clickedItem.getType());
+                // Check if the player has a spell book in hand
+                if (player.getInventory().getItemInMainHand().getType() == Material.BOOK) {
+                    // Add spell to player's inventory only if the spell book is in hand
+                    switch (spellName) {
+                        case "Fiery Wand":
+                        case "Teleportation Blade":
+                        case "Thunder Hammer":
+                        case "Magical Minecart":
+                        case "Gust Feather":
+                        case "Winged Shield":
+                        case "Big Man Slam":
+                            handleSpellAddition(player, clickedItem.getType());
 
-                        // close the inventory
-                        // cancel event to prevent item being moved in inventory
-                        player.closeInventory();
-                        event.setCancelled(true);
-                        break;
+                            // Close the inventory
+                            // Cancel event to prevent the item from being moved in the inventory
+                            player.closeInventory();
+                            event.setCancelled(true);
+                            break;
+                    }
+                } else {
+                    // If the player doesn't have a spell book in hand, just close the inventory
+                    player.closeInventory();
+                    event.setCancelled(true);
                 }
             }
         }
